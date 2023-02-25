e4ed649caded98b3c54b9109f8b9d02b96e9bae3
plowzzer <poloo.p3@gmail.com>
2023-02-25 00:53:56 -0300

    feat(start): working simple

---
+++ src/app/markdown-mock.js
@@ -0,0 +1,18 @@
+export default `
+  # Welcome to Markdown
+
+  Markdown is a lightweight markup language that you can use o add formatting elements to plaintext text documents.
+
+  ## How to use his?
+
+  1. Write markdown is the markdown editor window
+  2. See the rendered markdown in the preview window
+
+  ### Feature
+  
+  - Create heading, paragraphs, links, blockquotes, inline-code, code blocks, and lists
+  - Name and save the document to acess again laer
+  - Choose between Light and Dark mode depending on your preference
+
+  > This is an exeple of a blockquote. If you would like to learn more about markdown syntax, you can visit this [markdown cheatsheet](https://www.markdown.org.cheat-sheet).
+`
--- src/app/page.js
+++ src/app/page.js
@@ -1,90 +1,60 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+'use client'
+// import Image from 'next/image'
+import { Roboto_Slab, Source_Code_Pro } from 'next/font/google'
+import styles from './page.module.scss'
 
-const inter = Inter({ subsets: ['latin'] })
+import { useRef, useState, useEffect } from 'react';
+import Editor from '@monaco-editor/react';
+import { parse } from 'marked';
+
+import markdownMock from './markdown-mock'
+const robotoSlab = Roboto_Slab({ subsets: ['latin'] })
+const sourceCodePro = Source_Code_Pro({ subsets: ['latin'] })
 
 export default function Home() {
+  const editorRef = useRef(null);
+  const [code, setCode] = useState('')
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+    setText(markdownMock)
+    setCode(parse(markdownMock))
+  }, [])
+
+  const handleEditorDidMount = (editor, monaco) => {
+    editorRef.current = editor;
+  };
+
+  const handleEditorChange = (value) => {
+    const parsedCode = parse(value)
+    setCode(parsedCode)
+  }
+
+  const handleTextChange = (e) => {
+    const value = e.target.value
+    setText(value)
+    const parsedCode = parse(value)
+    setCode(parsedCode)
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className={[styles.editor, sourceCodePro.className].join(' ')}>
+        {/* <Editor
+          defaultLanguage="markdown"
+          defaultValue={markdownMock}
+          theme="vs-dark"
+          onMount={handleEditorDidMount}
+          onChange={handleEditorChange}
+        /> */}
+        <textarea
+          onChange={handleTextChange}
+          value={text}
+        >{text}</textarea>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={[styles.markdown, robotoSlab.className].join(' ')}>
+        <div dangerouslySetInnerHTML={{ __html: code }} />
       </div>
     </main>
   )
